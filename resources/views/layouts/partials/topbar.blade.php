7ec5088fd05276e974ca07ec33c211205b1e673f
Kautsar Al Bana <kautsaralbana@hotmail.com>
2020-10-10 03:40:00 +0700

    Update Top Bar

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -56,12 +56,6 @@
                     <i class="dripicons-menu"></i>
                 </button>
             </li>
-            <li class="hide-phone app-search">
-                <form role="search" class="">
-                    <input type="text" placeholder="Search..." class="form-control">
-                    <a href=""><i class="fa fa-search"></i></a>
-                </form>
-            </li>
         </ul>
     </nav>
 </div>
